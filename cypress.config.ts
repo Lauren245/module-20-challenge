083369608fbc7f35f67aaf372fe0da149d0a4f8e
Lauren Moore <102630328+Lauren245@users.noreply.github.com>
2025-03-16 19:33:46 -0700

    Revert "Unconfigured e2e tests"

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,4 +11,10 @@ export default defineConfig({
     },
   },
 
+  e2e: {
+    baseUrl: 'http://localhost:3001',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
